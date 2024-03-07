41c5c0aac17003cb58130184d1951fcad8b2eb80
zaldyprtm <zaldyprtm@gmail.com>
2024-03-07 15:51:19 +0700

    fix while

--- emyu.py
+++ emyu.py
@@ -1,40 +1,47 @@
 import random
 from libs import welcome_message
 
-emyu_posiion = random.randint(1, 4)
+while True:
+    emyu_position = random.randint(1, 4)
 
-welcome_message("SELAMAT DATANG DI GOA EMYU V1")
+    welcome_message("SELAMAT DATANG DI GOA EMYU V1")
 
-nama_fans = input("Masukan nama kamu: ")
-while nama_fans == "":
-    nama_fans = input("isi dulu nama anda: ")
+    nama_fans = input("Masukkan nama kamu: ")
+    while not nama_fans.strip():
+        nama_fans = input("Isi dulu nama anda: ")
 
-bentuk_goa = "|_|"
-goa_kosong = [bentuk_goa] * 4
+    bentuk_goa = "|_|"
+    goa_kosong = [bentuk_goa] * 4
 
-goa = goa_kosong.copy()
-goa[emyu_posiion - 1] = "|0_0|"
+    goa = goa_kosong.copy()
+    goa[emyu_position - 1] = "|0_0|"
 
-goa_kosong = '  '.join(goa_kosong)
-goa = '  '.join(goa)
+    goa_kosong = '  '.join(goa_kosong)
+    goa = '  '.join(goa)
 
-print(f''' Halo {nama_fans}! Coba perhatiin GOA dibawah ini {goa_kosong}''')
+    print(f'''Halo {nama_fans}! Coba perhatikan GOA di bawah ini:\n{goa_kosong}''')
 
-pilihan_fans = input("Menurut kamu di GOA nomor berapakah EMYU berada? [1 / 2 / 3 / 4]: ")
-while not pilihan_fans.isdigit() or int(pilihan_fans) not in [1, 2, 3, 4]:
-    pilihan_fans = input("Pilih dulu dong!! [1 / 2 / 3 / 4]: ")
-pilihan_fans = int(pilihan_fans)
+    pilihan_fans = input("Menurut kamu di GOA nomor berapakah EMYU berada? [1 / 2 / 3 / 4]: ")
+    while not pilihan_fans.isdigit() or int(pilihan_fans) not in [1, 2, 3, 4]:
+        pilihan_fans = input("Pilih dulu dong!! [1 / 2 / 3 / 4]: ")
+    pilihan_fans = int(pilihan_fans)
 
-confirm_answer = input(f"Kamu yakin jawabannya {pilihan_fans}? [y/n]: ")
+    confirm_answer = input(f"Kamu yakin jawabannya {pilihan_fans}? [y/n]: ")
 
-if confirm_answer == "n":
-    print("Program dihentikan")
-    exit()
-elif confirm_answer:
-    if pilihan_fans == emyu_posiion:
-        print(f"{goa} \n Selamat {nama_fans} Kamu benar! posisi EMYU ada di GOA nomor {emyu_posiion} dan pilihanmu adalah goa nomor {pilihan_fans}.")
+    if confirm_answer.lower() == "n":
+        print("Program dihentikan")
+        break
+    elif confirm_answer.lower() == "y":
+        if pilihan_fans == emyu_position:
+            print(f"{goa}\nSelamat {nama_fans}! Kamu benar! Posisi EMYU ada di GOA nomor {emyu_position} dan pilihanmu adalah GOA nomor {pilihan_fans}.")
+        else:
+            print(f"{goa}\nKamu KALAH!!! Seperti EMYU yang selalu KALAH, EMYU tidak berada di sana, tetapi ada di GOA nomor {emyu_position}, sedangkan kamu memilih GOA nomor {pilihan_fans}.")
     else:
-        print(f"{goa} \n KAMU KALAH!!! kayaak emyu yang selalu KALAH awkaokwo, EMYU bukan berada disitu tetapi ada di goa nomor {emyu_posiion}, sedangkan kamu memlih GOA nomor {pilihan_fans}")
-else:
-    print(f"Silakan ulangi programnya")
-    exit()
+        print("Input tidak valid.")
+        break
+
+    main_lagi = input("\n\nMau lanjut main game lagi? [y/n]")
+    if main_lagi.lower() == "n":
+        break
+
+print("Program Selesai")
